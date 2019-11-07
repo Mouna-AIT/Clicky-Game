7d209f92826e33aa3c442b084440dd1351f80269
Moumix <mouna.aitichou@gmail.com>
2019-11-06 22:30:22 -0500

    added container

--- Clicky-Gameapp/src/App.js
+++ Clicky-Gameapp/src/App.js
@@ -1,28 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
+import Jumbotron from "./components/Jumbotron";
+import MainGameContainer from "./components/MainGameContainer";
 import "./App.css";
-import Nav from "./components/navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Nav score={7} />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React today
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <div className="container-fluid mainContainer">
+    <Jumbotron />
+    <MainGameContainer /> 
+  </div>
+);
 
 export default App;
